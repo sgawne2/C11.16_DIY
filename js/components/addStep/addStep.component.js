e424b373fc18099ad55f9dacfc999aeacf14f1d4
milezperhour <hyperbolvoid@gmail.com>
2017-01-25 11:42:54 -0800

    added Angular components and moved script files

---
+++ js/components/addStep/addStep.component.js
@@ -0,0 +1,9 @@
+function addStepController(){
+
+}
+
+angular.module('diyApp').component('addSteps', {
+    templateUrl: './js/components/addStep/addStep.component.html',
+    controller: addStepController,
+    transclude: true
+});
