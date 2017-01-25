c6c62a299ddb39d7fc1a24c79df80588e7344773
milezperhour <hyperbolvoid@gmail.com>
2017-01-25 13:45:11 -0800

    Adding & deleting step functionallity working

--- js/components/addStep/addStep.component.js
+++ js/components/addStep/addStep.component.js
@@ -1,5 +1,21 @@
 function addStepController(){
+    var ctrl = this;
+    ctrl.steps = [{}];
 
+    ctrl.add = function(){
+        ctrl.steps.push({});
+    };
+
+    ctrl.delete = function(step){
+        var index = ctrl.steps.indexOf(step);
+
+        if (ctrl.steps.length > 1) {
+            ctrl.steps.splice(index, 1);
+        }else{
+            ctrl.steps[index] = {};
+        }
+
+    }
 }
 
 angular.module('diyApp').component('addSteps', {
