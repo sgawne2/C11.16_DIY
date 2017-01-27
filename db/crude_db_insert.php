c91947f16bc5e66efac7cc9a7fca5f6589868421
Vernon Louie <vernonlouie@aol.com>
2017-01-26 17:20:35 -0800

    finished insert of tool_id & project_id -VL & SG

--- db/crude_db_insert.php
+++ db/crude_db_insert.php
@@ -4,31 +4,20 @@
     /* mysql_connect.php has $conn, which uses mysqli_connect */
     require('gyver_connect.php');
 
-    // insert into "projects" table ***************************************************
+    // INSERT INTO "PROJECTS" TABLE ***************************************************
     $proj_name = $_POST["proj_name"];
     $main_photo = $_POST["main_photo"];
 
+        // get tool_count, because it also goes into "projects" table
     $tool_count = count($_POST["tools"]);
     print("tool count: ".$tool_count);
-
-    $string_of_tools =  "'" . implode("', '", $_POST["tools"]). "'";
-    $query = "SELECT `tool_id` FROM `tools` WHERE tool_name IN ($string_of_tools) ORDER BY `tool_id` ASC";
-
-    $result = mysqli_query($conn, $query);    // select action
-    $output = [];
-
-    while ($row = mysqli_fetch_assoc($result)) {
-        $output[] = $row;
-    }
-
-    print_r($output);
 
     $query_proj = "INSERT INTO `projects` SET
                 `project_name` = '$proj_name',
                 `tool_count` = $tool_count,
                 `project_photo` = '$main_photo' ";
 
-    print("\n".$query_proj);
+//    print("\n".$query_proj);
     $result_proj = mysqli_query($conn, $query_proj);    // insert action
 
     if (mysqli_affected_rows($conn) > 0) {      // get project_id of newly inserted project = $id
@@ -39,7 +28,7 @@
     }
 
 
-    // insert into "p_instructions" table ***************************************************
+    // INSERT INTO "P_INSTRUCTIONS" TABLE ***************************************************
     $stp1_num = $_POST["stp1_num"];
     $stp1_name = $_POST["stp1_name"];
     $stp1_txt = $_POST["stp1_txt"];
@@ -50,18 +39,18 @@
     $stp2_txt = $_POST["stp2_txt"];
     $stp2_pic = $_POST["stp2_pic"];
 
-        // IGNORE allows for inserting multiple records
+        // IGNORE allows for inserting multiple records  ALSO $id doesn't come from $_POST, rather from my_sqli_affected_row()
     $query_p_instructions = "INSERT IGNORE INTO `p_instructions`   
         (`project_id`, `step_number`, `step_name`, `step_text`, `step_photo`)
     VALUES 
         (($id), ($stp1_num), ('$stp1_name'), ('$stp1_txt'), ('$stp1_pic')),
         (($id), ($stp2_num), ('$stp2_name'), ('$stp2_txt'), ('$stp2_pic')) ";
 
-    print("\n".$query_p_instructions);
+//    print("\n".$query_p_instructions);
     $result_p_instructions = mysqli_query($conn, $query_p_instructions);    // insert action
 
 
-    // insert into "tools" table ***************************************************
+    // INSERT INTO "TOOLS" TABLE ***************************************************
     $tool1 = $_POST["tools"]["tool1"];
     $tool2 = $_POST["tools"]["tool2"];
     $tool3 = $_POST["tools"]["tool3"];
@@ -73,6 +62,39 @@
 
     print("\n".$query_tools);
     $result_tools = mysqli_query($conn, $query_tools);  // insert action
+
+    // INSERT INTO "MAP_TP" TABLE ***************************************************
+        // "implode" makes a single string out of array elements separated by whatever is in double quotes
+    $string_of_tools =  "'" . implode("', '", $_POST["tools"]). "'";
+    $query = "SELECT `tool_id` FROM `tools` WHERE tool_name IN ($string_of_tools) ORDER BY `tool_id` ASC";
+    $result = mysqli_query($conn, $query);    // select action
+
+    $tool_id_arr = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $tool_id_arr[] = $row;
+    }
+    print_r($tool_id_arr);
+
+//    $query_p_maptp = "INSERT IGNORE INTO `map_tp`
+//            (`project_id`, `tool_id`)
+//        VALUES
+//            (($id), ($)),
+//            (($id), ($stp2_num)) ";
+
+
+
+    for ($i = 0; $i < count($tool_id_arr); $i++) {
+        $output[] = "(". $id . ", " . $tool_id_arr[$i]['tool_id'] . ")" ;
+    }
+
+    $string_of_tool_IDs = implode(", ", $output);
+    $query_map_tp = "INSERT INTO `map_tp` 
+        (`project_id`, `tool_id`) 
+    VALUES 
+        $string_of_tool_IDs ";
+
+    print_r("\n".$query_map_tp);
+    $result = mysqli_query($conn, $query_map_tp);    // insert action
 
 
 ?>
