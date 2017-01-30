db8515a12aa0cf8004f38fe5aad5e8d4a8de554a
sgawne2 <sgawne2@gmail.com>
2017-01-30 15:33:24 -0800

    view project page gets data with angular

--- js/components/viewProject/viewProject.component.js
+++ js/components/viewProject/viewProject.component.js
@@ -1,30 +1,41 @@
-function viewProjectController(){
+function viewProjectController($http, $location){
     var ctrl = this;
 
-    ctrl.project_info = {
-        project_name: 'CPU Drone',
-        project_photo: 'images/drone.jpg',
-        project_description: 'This is a drone.'
-    };
-
-    ctrl.tools = [
-        {name: "Hammer"},
-        {name:"Nails"},
-        {name:"Self Respect (optional)"}
-        ];
-
-    ctrl.steps = [{
-        step_number: 1,
-        description: "My grandma used to make these all the time. Steal CPUS fans from where ever",
-        step_photo: 'images/fan.jpg'
-        },
-        {
-        step_number: 2,
-        description: "Your drone is now complete. Nice job",
-        step_photo: 'images/drone.jpg'
-        }];
+    // ctrl.project_info = {
+    //     project_name: 'CPU Drone',
+    //     project_photo: 'images/drone.jpg',
+    //     project_description: 'This is a drone.'
+    // };
+    //
+    // ctrl.tools = [
+    //     {name: "Hammer"},
+    //     {name:"Nails"},
+    //     {name:"Self Respect (optional)"}
+    //     ];
+    //
+    // ctrl.steps = [{
+    //     step_number: 1,
+    //     description: "My grandma used to make these all the time. Steal CPUS fans from where ever",
+    //     step_photo: 'images/fan.jpg'
+    //     },
+    //     {
+    //     step_number: 2,
+    //     description: "Your drone is now complete. Nice job",
+    //     step_photo: 'images/drone.jpg'
+    //     }];
 
     ctrl.step_list = [];
+    var pid = location.search.split('pid=')[1];
+    $http({
+        method: 'POST',
+        data: {pid: pid},
+        url: "project_details.php"
+    })
+        .then(function(response) {
+            ctrl.project_info = response.data.info;
+            ctrl.tools = response.data.tools;
+            ctrl.steps = response.data.steps;
+        });
 }
 
 angular.module('diyApp').component('viewProject', {
