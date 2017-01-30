a50898d093a3d9fcc65183d88b3c57270f57e808
sgawne2 <sgawne2@gmail.com>
2017-01-29 18:20:21 -0800

    early chips functionality

--- js/components/autoChip/autoChip.component.js
+++ js/components/autoChip/autoChip.component.js
@@ -1,44 +1,34 @@
-function DemoCtrl ($timeout, $q) {
-    var ctrl = this;
-
-    ctrl.readonly = false;
-    ctrl.selectedItem = null;
-    ctrl.searchText = null;
-    ctrl.querySearch = ctrl.querySearchF();
-    ctrl.vegetables = ctrl.loadVegetables();
-    ctrl.selectedVegetables = [];
-    ctrl.numberChips = [];
-    ctrl.numberChips2 = [];
-    ctrl.numberBuffer = '';
-
-    /**
-     * Search for vegetables.
-     */
-    ctrl.querySearchF = function(query) {
-        var results = query ? ctrl.vegetables.filter(createFilterFor(query)) : [];
-        return results;
-    };
-
-    /**
-     * Create filter function for a query string
-     */
-    ctrl.createFilterFor = function(query) {
-        var lowercaseQuery = angular.lowercase(query);
-
-        return function filterFn(vegetable) {
-            return (vegetable.toLowerCase().indexOf(lowercaseQuery) === 0);
+function chipController() {
+    var self = this;
+    self.readonly = false;
+    // Lists of fruit names and Vegetable objects
+    self.fruitNames = ['Apple', 'Banana', 'Orange'];
+    self.roFruitNames = angular.copy(self.fruitNames);
+    self.tags = [];
+    self.vegObjs = [
+        {
+            'name' : 'Broccoli',
+            'type' : 'Brassica'
+        },
+        {
+            'name' : 'Cabbage',
+            'type' : 'Brassica'
+        },
+        {
+            'name' : 'Carrot',
+            'type' : 'Umbelliferous'
+        }
+    ];
+    self.newVeg = function(chip) {
+        return {
+            name: chip,
+            type: 'unknown'
         };
-
     };
-
-    ctrl.loadVegetables = function() {
-        var veggies = ['Broccoli','Cabbage','Carrot', 'Lettuce','Spinach'];
-        return veggies;
-    }
 }
 
 angular.module('diyApp').component('autoChip', {
     templateUrl: './js/components/autoChip/autoChip.component.html',
-    controller: DemoCtrl
+    controller: chipController
 });
 
