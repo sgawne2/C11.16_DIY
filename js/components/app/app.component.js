f14d7d5b20f5cee5b334952079bef0e2ef47f7ea
sgawne2 <sgawne2@gmail.com>
2017-01-30 00:34:18 -0800

    fixed result display

--- js/components/app/app.component.js
+++ js/components/app/app.component.js
@@ -1,41 +1,41 @@
 function appController($http) {
     var ctrl = this;
-    ctrl.data = [{
-        project_name: 'CPU Drone',
-        project_photo: 'images/drone.jpg',
-        project_description: 'This is a drone.',
-        owned_tools: null,
-        req_count: null,
-        own_count: null,
-        score: null
-    },
-    {
-        project_name: 'Industrial Pipe Lamp',
-        project_photo: 'images/lamp.jpg',
-        project_description: 'This is a pipe lamp thing.',
-        owned_tools: null,
-        req_count: null,
-        own_count: null,
-        score: null
-    },
-    {
-        project_name: 'Homemade Lava Lamp',
-        project_photo: 'images/lava_lamp.jpg',
-        project_description: 'Aww yeaaa.',
-        owned_tools: null,
-        req_count: null,
-        own_count: null,
-        score: null
-    },
-    {
-        project_name: 'Broken Glass Project',
-        project_photo: 'images/broken_glass.jpg',
-        project_description: 'This project uses broken glass.',
-        owned_tools: null,
-        req_count: null,
-        own_count: null,
-        score: null
-    }];
+    // ctrl.data = [{
+    //     project_name: 'CPU Drone',
+    //     project_photo: 'images/drone.jpg',
+    //     project_description: 'This is a drone.',
+    //     owned_tools: null,
+    //     req_count: null,
+    //     own_count: null,
+    //     score: null
+    // },
+    // {
+    //     project_name: 'Industrial Pipe Lamp',
+    //     project_photo: 'images/lamp.jpg',
+    //     project_description: 'This is a pipe lamp thing.',
+    //     owned_tools: null,
+    //     req_count: null,
+    //     own_count: null,
+    //     score: null
+    // },
+    // {
+    //     project_name: 'Homemade Lava Lamp',
+    //     project_photo: 'images/lava_lamp.jpg',
+    //     project_description: 'Aww yeaaa.',
+    //     owned_tools: null,
+    //     req_count: null,
+    //     own_count: null,
+    //     score: null
+    // },
+    // {
+    //     project_name: 'Broken Glass Project',
+    //     project_photo: 'images/broken_glass.jpg',
+    //     project_description: 'This project uses broken glass.',
+    //     owned_tools: null,
+    //     req_count: null,
+    //     own_count: null,
+    //     score: null
+    // }];
 
     ctrl.getResult = function(string) {
         $http({
