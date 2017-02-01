8dbcea37b197bddc6feed1611f2b5ec86a3ac927
milezperhour <hyperbolvoid@gmail.com>
2017-02-01 11:44:33 -0800

    added an insert photo required on submit project page

--- js/components/addPhoto/addPhoto.component.js
+++ js/components/addPhoto/addPhoto.component.js
@@ -1,10 +1,48 @@
+function addPhotoController($scope){
+    var ctrl = this;
+
+    $('#fileUpload').on('change', function(e) {
+        var files = e.target.files;
+        if (files[0]) {
+            ctrl.fileName = files[0].name;
+        } else {
+            ctrl.fileName = null;
+        }
+
+        ctrl.loadedPhoto = ctrl.fileName;
+        console.log(ctrl.loadedPhoto);
+        console.log(e.target.files);
+        $scope.$apply();
+
+        var preview = document.querySelector('img');
+        var file    = document.querySelector('input[type=file]').files[0];
+        var reader  = new FileReader();
+
+        reader.addEventListener("load", function () {
+            // ctrl.photos[index] = reader.result;
+            preview.src = reader.result;
+        }, false);
+
+        if (files[0]) {
+            reader.readAsDataURL(files[0]);
+
+            ctrl.photos = [];
+            ctrl.photos.push(ctrl.fileName);
+            console.log("ctrl.photos array: ", ctrl.photos);
+        }
+
+    });
+
+
+}
+
 // function addPhotoController($scope){
 //     var ctrl = this;
 //
 //     $('#fileUpload').on('change', function(e) {
-//         var files = e.target.files;
-//         if (files[0]) {
-//             ctrl.fileName = files[0].name;
+//         ctrl.files = e.target.files;
+//         if (ctrl.files[0]) {
+//             ctrl.fileName = ctrl.files[0].name;
 //         } else {
 //             ctrl.fileName = null;
 //         }
@@ -16,7 +54,7 @@
 //
 //
 //         var preview = document.querySelector('img');
-//         var file    = document.querySelector('input[type=file]').files[0];
+//         ctrl.file    = document.querySelector('input[type=file]').files[0];
 //         var reader  = new FileReader();
 //
 //         reader.addEventListener("load", function () {
@@ -24,53 +62,15 @@
 //             preview.src = reader.result;
 //         }, false);
 //
-//         if (files[0]) {
-//             reader.readAsDataURL(files[0]);
+//         if (ctrl.files[0]) {
+//             reader.readAsDataURL(ctrl.files[0]);
 //         }
 //
 //     });
 //
 //     ctrl.photos = [];
 //
-//
 // }
-
-function addPhotoController($scope){
-    var ctrl = this;
-
-    $('#fileUpload').on('change', function(e) {
-        ctrl.files = e.target.files;
-        if (ctrl.files[0]) {
-            ctrl.fileName = ctrl.files[0].name;
-        } else {
-            ctrl.fileName = null;
-        }
-
-        ctrl.loadedPhoto = ctrl.fileName;
-        console.log(ctrl.loadedPhoto);
-        console.log(e.target.files);
-        $scope.$apply();
-
-
-        var preview = document.querySelector('img');
-        ctrl.file    = document.querySelector('input[type=file]').files[0];
-        var reader  = new FileReader();
-
-        reader.addEventListener("load", function () {
-            // ctrl.photos[index] = reader.result;
-            preview.src = reader.result;
-        }, false);
-
-        if (ctrl.files[0]) {
-            reader.readAsDataURL(ctrl.files[0]);
-        }
-
-    });
-
-    ctrl.photos = [];
-
-
-}
 
 angular.module('diyApp').component('addPhoto', {
     templateUrl: './js/components/addPhoto/addPhoto.component.html',
--- js/components/addStep/addStep.component.js
+++ js/components/addStep/addStep.component.js
@@ -1,4 +1,4 @@
-function addStepController($scope){
+function addStepController(){
     var ctrl = this;
     ctrl.steps = [{}];
 
