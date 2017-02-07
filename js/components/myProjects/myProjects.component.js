8133462b03c5c2a9e66a1e721f9cc9efcf515089
milezperhour <hyperbolvoid@gmail.com>
2017-02-06 17:53:27 -0800

    changed height propery on leftProfileContent

---
+++ js/components/myProjects/myProjects.component.js
@@ -0,0 +1,10 @@
+function myProjectsController(){
+
+}
+
+
+angular.module('diyApp').component('myProjects', {
+    templateUrl: './js/components/myProjects/myProjects.component.html',
+    controller: myProjectsController,
+    transclude: true
+});
