c9604e863ad8daf032f080dbf7279641ccff25cc
milezperhour <hyperbolvoid@gmail.com>
2017-02-08 15:49:32 -0800

    added images for the index page

--- index.php
+++ index.php
@@ -123,10 +123,22 @@
     </div>
 </md-toolbar>
 
+
 <div class="headerImage" hide-xs hide-gt-xs hide-sm hide-gt-sm show-md show-gt-md show-lg show-gt-lg show-xl>
     <h2 class="headerImageText"><span class="orangeText">Mac</span><span class="darkTealText">diy</span><span class="orangeText">ver</span></h2>
     <h1>Let's make some stuff!</h1>
 </div>
+
+<!--<div class="headerImage" hide-xs hide-gt-xs hide-sm hide-gt-sm show-md show-gt-md show-lg show-gt-lg show-xl>-->
+<!--    <img class="image2" src="images/technology1.jpg">-->
+<!--<!--    <img class="image1" src="images/tool-box.jpg">-->
+<!--<!--    <img class="image3" src="images/art_supplies.jpg">-->
+<!--</div>-->
+<!---->
+<!--<div class="homepageTextContainer" layout-align="center">-->
+<!--    <h2 class="headerImageText"><span class="orangeText">Mac</span><span class="darkTealText">diy</span><span class="orangeText">ver</span></h2>-->
+<!--    <h1>Let's make some stuff!</h1>-->
+<!--</div>-->
 
 <!--&lt;!&ndash;search bar&ndash;&gt;-->
 <!--<auto-chip></auto-chip>-->
@@ -141,5 +153,6 @@
 
 <!--side nav-->
 <side-panel></side-panel>
+
 </body>
 </html>
--- js/components/featuredProjects/featuredProjects.component.js
+++ js/components/featuredProjects/featuredProjects.component.js
@@ -1,47 +1,46 @@
 function featuredProjectsController(){
     var ctrl = this;
-    // ctrl.data = [
-    // ];
+
     ctrl.featuredData = [{
         project_id: 4,
-        project_name: 'CPU Drone',
-        project_photo: 'images/drone.jpg',
-        project_description: 'This is a drone.',
+        project_name: 'Crate Coffee Table',
+        project_photo: 'images/table.jpg',
+        project_description: 'Create an awesome table from crates.',
         owned_tools: null,
         own_count: 4,
         req_count: 7,
         score: null
     },
-        {
-            project_id: 5,
-            project_name: 'Industrial Pipe Lamp',
-            project_photo: 'images/lamp.jpg',
-            project_description: 'This is a pipe lamp thing.',
-            owned_tools: null,
-            own_count: 5,
-            req_count: 10,
-            score: null
-        },
-        {
-            project_id: 6,
-            project_name: 'Homemade Lava Lamp',
-            project_photo: 'images/lava_lamp.jpg',
-            project_description: 'Aww yeaaa.',
-            owned_tools: null,
-            own_count: 1,
-            req_count: 5,
-            score: null
-        },
-        {
-            project_id: 7,
-            project_name: 'Broken Glass Project',
-            project_photo: 'images/broken_glass.jpg',
-            project_description: 'This project uses broken glass.',
-            owned_tools: null,
-            own_count: 2,
-            req_count: 3,
-            score: null
-        }];
+    {
+        project_id: 5,
+        project_name: 'Industrial Pipe Lamp',
+        project_photo: 'images/lamp.jpg',
+        project_description: 'This is a pipe lamp thing.',
+        owned_tools: null,
+        own_count: 5,
+        req_count: 10,
+        score: null
+    },
+    {
+        project_id: 6,
+        project_name: 'Homemade Lava Lamp',
+        project_photo: 'images/lava_lamp.jpg',
+        project_description: 'Aww yeaaa.',
+        owned_tools: null,
+        own_count: 1,
+        req_count: 5,
+        score: null
+    },
+    {
+        project_id: 7,
+        project_name: 'Broken Glass Project',
+        project_photo: 'images/broken_glass.jpg',
+        project_description: 'This project uses broken glass.',
+        owned_tools: null,
+        own_count: 2,
+        req_count: 3,
+        score: null
+    }];
 }
 
 angular.module('diyApp').component('featuredProjects', {
