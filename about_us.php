394d9e51fec17d46d7f2698e40952d81e60aebdb
milezperhour <hyperbolvoid@gmail.com>
2017-02-09 15:25:39 -0800

    changed content of About Us page

--- about_us.php
+++ about_us.php
@@ -62,10 +62,10 @@ session_start();
 
     <md-card-content class="aboutUsCard">
         <h3>You got trash? No, friend, you've got treasure.</h3>
-        <p>Hi, we're Sean, Vernon, and Miles, and we're proud to bring you MacDIYver, a website dedicated to showing you that with some effort, a good plan, and a little broken glass, you can make some truly amazing things. We're a passionate group of hackers and makers, and we'd love to help you express your own creativity and maker spirit.</p>
+        <p>Hi, we're Sean, Vernon, and Miles, and we're proud to bring you <em>Macdiyver</em>, a website dedicated to showing you that with some effort, a good plan, and a little broken glass, you can make some truly amazing things. We're a passionate group of hackers and makers, and we'd love to help you express your own creativity and maker spirit.</p>
         <h3>You provide the tools, and we provide the plan!</h3>
-        <p>You tell MacDIYver the tools and materials that you have available, and MacDIYver will help you find the perfect project, so that you can spend more time making and less time making excuses.</p>
-        <p>We created MacDIYver as our final project for the LearningFuze Full Immersion Web Development Bootcamp as a culmination of all of the technologies that we've learned about over the past twelve weeks. Our front end utilizes Angular and [stuff I don't know that you should answer yourself] in order to deliver a dynamic and responsive user experience. Our back end [does stuff I have no idea].</p>
+        <p>You tell <em>Macdiyver</em> the tools and materials that you have available, and <em>Macdiyver</em> will help you find the perfect project, so that you can spend more time making and less time making excuses.</p>
+        <p>We created <em>Macdiyver</em> as our final project for the LearningFuze Full Immersion Web Development Bootcamp as a culmination of all of the technologies that we've learned about over the past twelve weeks. Our front end utilizes Angular in order to deliver a dynamic and responsive user experience. Our back end is in PHP.</p>
         <h2>Meet the Team!</h2>
         <h3>Sean</h3>
         <p>The Full-Stack Developer</p>
