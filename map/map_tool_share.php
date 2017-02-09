7bdc41b32fedc29fef1d301cd7e3c3e0dc0a203f
Vernon Louie <vernonlouie@aol.com>
2017-02-08 19:50:09 -0800

    got users with tools on a google map, yeah!! -VL

--- map/map_tool_share.php
+++ map/map_tool_share.php
@@ -1,51 +1,35 @@
 
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
     <head>
-        <link type="text/css" rel="stylesheet" href="map_style.css" />
-        <script src="map.js"></script>
+        <title>Geocoding service</title>
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+        <meta charset="utf-8">
 
         <!-- include the jQuery library as we are using jQuery functions -->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script>
-            $(document).ready(function() {
-                $("#submitButton").click(function() {
-                    console.log("inside click handler");
-                    $.ajax({
-                        data:       $("#find_tool_form").serialize(),  // Serialize grabs the text from a form element
-                        dataType:   'text',
-                        url:        'find_tool_owners.php',
-                        method:     'post',
-                        success: function(result) {
-                            console.log("success!");
-                            console.log(result);    // result returns anything in html, anything that gets printed
-                        },
-                        error: function() {
-                            console.log("failure");
-                            console.log(result);
-                        }
-                    })
-                });
-            });
-        </script>
 
-        <meta charset="UTF-8">
-        <title>map toolShare</title>
+        <script src="js_map_tool_share.js"></script>
+
+        <link type="text/css" rel="stylesheet" href="map_stylesheet.css"/>
     </head>
 
     <body>
-        <form id="find_tool_form">
-            Tool name:
+        <form id="tool-form">
+            <br> What tool do you want to search for?
             <input type="text" name="tool_name">
+
             <button type="button" id="submitButton">submit</button>
         </form>
 
-        <div>
-            <input id="address" type="textbox" value="Sydney, NSW">
-            <input type="button" value="Geocode" onclick="codeAddress()">
-        </div>
-        <div id="map-canvas" style="height:90%;top:30px"></div>
+<!--        <div id="floating-panel">-->
+<!--            <input id="address" type="textbox" value="Sydney, NSW">-->
+<!--            <input id="submit" type="button" value="Geocode">-->
+<!--        </div>-->
+        <div id="map"></div>
 
+        <script async defer
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDrs1E9ETofrHeLWg27W6_eHO9Ky6fmuus&callback=initMap">
+        </script>
     </body>
 </html>
