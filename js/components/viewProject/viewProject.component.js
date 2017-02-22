60649cb96288240661952641be060af2a319ec70
milezperhour <hyperbolvoid@gmail.com>
2017-02-21 19:32:02 -0800

    removed commented out code from viewProject

--- js/components/viewProject/viewProject.component.js
+++ js/components/viewProject/viewProject.component.js
@@ -1,29 +1,6 @@
 function viewProjectController($http, $location){
     var ctrl = this;
 
-    // ctrl.project_info = {
-    //     project_name: 'CPU Drone',
-    //     project_photo: 'images/drone.jpg',
-    //     project_description: 'This is a drone.'
-    // };
-    //
-    // ctrl.tools = [
-    //     {name: "Hammer"},
-    //     {name:"Nails"},
-    //     {name:"Self Respect (optional)"}
-    //     ];
-    //
-    // ctrl.steps = [{
-    //     step_number: 1,
-    //     description: "My grandma used to make these all the time. Steal CPUS fans from where ever",
-    //     step_photo: 'images/fan.jpg'
-    //     },
-    //     {
-    //     step_number: 2,
-    //     description: "Your drone is now complete. Nice job",
-    //     step_photo: 'images/drone.jpg'
-    //     }];
-
     ctrl.step_list = [];
     var pid = location.search.split('pid=')[1];
     $http({
